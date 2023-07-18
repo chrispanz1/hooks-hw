be8452c3563f88d244a40a9f85a1bcd2d9eb2d46
chrispanz1 <133619340+chrispanz1@users.noreply.github.com>
2023-07-18 11:01:17 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState,useEffect} from 'react';
 function App() {
+  const [count,setCount]=useState(0)
+  const[evenCount,setEvenCount]=useState(0)
+  //-----------declare a new use state--//
+  const[name,setName]=useState('pete');
+  
+  const addone=()=>{
+    setCount(count+1)
+    if(count%2===0)
+    setEvenCount(evenCount+1)
+
+  }
+  const minusOne=()=>{
+    setCount(count-1)
+  }
+  useEffect(()=>{
+    alert("your boss changed");
+    
+  },[name]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>counter</h1>
+      <h3> {count}</h3>
+      <button onClick={()=>addone()}> addOne if you like retail!</button>
+      <button onClick={()=>minusOne()}> subtract one if you dislike retail!</button>
+      <button onClick={()=>setName('george')}>change boss</button>
+      <p>{name}</p>
+      <h3>Even Count:{evenCount}</h3>
     </div>
   );
 }
